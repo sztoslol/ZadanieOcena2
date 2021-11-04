f7dce90e7941e9786bc714b128dbb851d2711041
sztoslol <kubuszeski05@wp.pl>
2021-11-04 13:38:11 +0100

    w trakcie

--- app/src/main/java/com/example/zadanieocena/MainActivity.kt
+++ app/src/main/java/com/example/zadanieocena/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.zadanieocena
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var skladnik1 = 0
+        var skladnik2 = 0
+        var suma2 = 0
 
         findViewById<Button>(R.id.wyswietl).setOnClickListener {
             findViewById<TextView>(R.id.wyniktext).text = findViewById<TextView>(R.id.poleT).text.toString()
@@ -18,7 +24,10 @@ class MainActivity : AppCompatActivity() {
             findViewById<TextView>(R.id.wyniktext).text = "wynik: " + findViewById<TextView>(R.id.poleT).text.toString()
         }
         findViewById<Button>(R.id.suma).setOnClickListener {
-           // findViewById<TextView>(R.id)////
+            skladnik1 = findViewById<TextInputEditText>(R.id.LICZBA).toString().toInt()
+            //skladnik2 = findViewById<TextInputEditText>(R.id.dodawanie2).toString().toInt()
+           // suma2 = skladnik1 + skladnik1
+            findViewById<TextView>(R.id.wyniktext).text = suma2.toString()
         }
     }
 }
