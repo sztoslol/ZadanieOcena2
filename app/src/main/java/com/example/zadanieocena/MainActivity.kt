b354f11814b77841b66b79314b86c3094179df67
sztoslol <kubuszeski05@wp.pl>
2021-11-07 12:37:05 +0100

    Gotowe :)

--- app/src/main/java/com/example/zadanieocena/MainActivity.kt
+++ app/src/main/java/com/example/zadanieocena/MainActivity.kt
@@ -5,8 +5,6 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,19 +13,19 @@ class MainActivity : AppCompatActivity() {
 
         var skladnik1 = 0
         var skladnik2 = 0
-        var suma2 = 0
+        var suma = 0
 
-        findViewById<Button>(R.id.wyswietl).setOnClickListener {
-            findViewById<TextView>(R.id.wyniktext).text = findViewById<TextView>(R.id.poleT).text.toString()
-        }
         findViewById<Button>(R.id.zastap).setOnClickListener {
-            findViewById<TextView>(R.id.wyniktext).text = "wynik: " + findViewById<TextView>(R.id.poleT).text.toString()
+            findViewById<TextView>(R.id.wyniktext).text = findViewById<EditText>(R.id.poleT).text
+        }
+        findViewById<Button>(R.id.laczenie).setOnClickListener {
+            findViewById<TextView>(R.id.wyniktext).text = findViewById<TextView>(R.id.wyniktext).text.toString() + findViewById<EditText>(R.id.poleT).text
         }
         findViewById<Button>(R.id.suma).setOnClickListener {
-            skladnik1 = findViewById<TextInputEditText>(R.id.LICZBA).toString().toInt()
-            //skladnik2 = findViewById<TextInputEditText>(R.id.dodawanie2).toString().toInt()
-           // suma2 = skladnik1 + skladnik1
-            findViewById<TextView>(R.id.wyniktext).text = suma2.toString()
+            skladnik1 = findViewById<EditText>(R.id.LICZBA).text.toString().toInt()
+            skladnik2 = findViewById<EditText>(R.id.LICZBA2).text.toString().toInt()
+            suma = skladnik1 + skladnik2
+            findViewById<TextView>(R.id.wyniktext).text = suma.toString()
         }
     }
 }
